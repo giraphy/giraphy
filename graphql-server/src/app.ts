7717b815c553e2c59a1a6e451665d774893ed7d2
YuitoSato <yuitosato.w@gmail.com>
2020-07-06 21:48:01 +0900

    :+1: wip

--- graphql-server/src/app.ts
+++ graphql-server/src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import graphqlHTTP from "express-graphql";
 
 import * as core from "express-serve-static-core";
@@ -9,52 +9,63 @@ import { authorize } from "./authorizer/authorizer";
 import { permissionPolicy } from "./conf/permission-policy";
 import { authenticate } from "./authenticator/authenticator";
 
-class App {
-  public express: core.Express;
+export const app = (request: Request, response: Response) => {
 
-  constructor() {
-    this.express = express();
-    this.express.use(bodyParser.urlencoded({ extended: true }));
-    this.express.use(bodyParser.text({ type: "application/graphql" }));
-    this.express.use(bodyParser.json());
-    this.mountRoutes();
-  }
 
-  private mountRoutes(): void {
-    const router = express.Router();
 
-    router.post("/graphql", authenticate, (req, res) => {
-      const document = parse(new Source(req.body["query"]));
-      const context = (req as any)["user"] ? (req as any)["user"] : {};
-      try {
-        if (permissionPolicy) {
-          authorize(document, context, permissionPolicy);
-        }
-        return graphqlHTTP({
-          schema: schema,
-          graphiql: true,
-        })(req, res);
-      } catch (e) {
-        if (e.name === "ForbiddenError") {
-          res.status(403);
-          res.send({
-            errors: [
-              {
-                message: e.message,
-                locations: [],
-                path: [],
-                extensions: {
-                  code: e.name,
-                },
-              },
-            ],
-          });
-        }
-        throw e;
-      }
-    });
-    this.express.use("/", router);
-  }
-}
+  return graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+};
 
-export default new App().express;
+
+// class App {
+//   public express: core.Express;
+//
+//   constructor() {
+//     this.express = express();
+//     this.express.use(bodyParser.urlencoded({ extended: true }));
+//     this.express.use(bodyParser.text({ type: "application/graphql" }));
+//     this.express.use(bodyParser.json());
+//     this.mountRoutes();
+//   }
+//
+//   private mountRoutes(): void {
+//     const router = express.Router();
+//
+//     router.post("/graphql", authenticate, (req, res) => {
+//       const document = parse(new Source(req.body["query"]));
+//       const context = (req as any)["user"] ? (req as any)["user"] : {};
+//       try {
+//         if (permissionPolicy) {
+//           authorize(document, context, permissionPolicy);
+//         }
+//         return graphqlHTTP({
+//           schema: schema,
+//           graphiql: true,
+//         })(req, res);
+//       } catch (e) {
+//         if (e.name === "ForbiddenError") {
+//           res.status(403);
+//           res.send({
+//             errors: [
+//               {
+//                 message: e.message,
+//                 locations: [],
+//                 path: [],
+//                 extensions: {
+//                   code: e.name,
+//                 },
+//               },
+//             ],
+//           });
+//         }
+//         throw e;
+//       }
+//     });
+//     this.express.use("/", router);
+//   }
+// }
+//
+// export default new App().express;
--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -1,7 +1,7 @@
-import app from "./app";
+import { schema } from './schema/schema';
+import graphqlHTTP = require('express-graphql');
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  return console.log(`server is listening on ${port}`);
+export default graphqlHTTP({
+  schema,
+  graphiql: true
 });
